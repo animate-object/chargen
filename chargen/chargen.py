fe213916004c41c2072e1c0f659388d8708d623d
Alex Prinsen <alexander.f.prinsen@gmail.com>
2018-02-04 01:26:57 -0600

    working subclasses and stats

--- chargen/chargen.py
+++ chargen/chargen.py
@@ -1,29 +1,47 @@
 #! /usr/bin/env python3
 
-from constants import CLASSES, RACES, BACKGROUNDS
-from random import choice, seed
+from constants import CLASSES, RACES, BACKGROUNDS, SUBCLASSES_BY_CLASS
+from random import choice, seed, randint
 from json import dumps
 
-def generate_character():
+STAT_METHOD = ['3d6']
+ABILITIES = ['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA']
+
+def generate_character(random_assign_stats=False):
     """ Generate data for a DnD character
     """
     class_ = choice(CLASSES)
+    subclass = choice(SUBCLASSES_BY_CLASS[class_])
     race = choice(RACES)
     background = choice(BACKGROUNDS)
 
+    # stats
+    stats = roll_stats('3d6')
+    if random_assign_stats:
+        stats = assign_stats(stats)
+
     return {
-        'class': class_,
-        'race': race,
-        'background': background
+        'class': class_.capitalize(),
+        'subclass': subclass,
+        'race': race.capitalize(),
+        'background': background.capitalize(),
+        'stats': stats
     }
 
+def roll_stats(strategy):
+    """ Roll level 1 base stats (does not apply racial bonuses)
+    """
+    if not strategy == '3d6':
+        raise NotImplemented("Strategy {} not implemented".format(strategy))
+    return [sum([randint(1,6) for _ in range(3)]) for _ in range(6)]
 
-
+def assign_stats(stats):
+    return dict(zip(ABILITIES, stats))
 
 def print_character(char_data):
     pass
 
 if __name__ == '__main__':
     seed()
-    char_data = generate_character()
+    char_data = generate_character(random_assign_stats=True)
     print(dumps(char_data))
--- chargen/constants.py
+++ chargen/constants.py
@@ -1,4 +1,4 @@
-# -------------------------------------------------------- #
+#i -------------------------------------------------------- #
 #   class constants
 # ------------------
 #   there are 12 character classes, all specified in the DMG
@@ -24,8 +24,7 @@ WIZARD = 'wizard'
 #   supplemental races are specified in volos
 #   which categorizes monstrous races separately
 #   there are apparently also wizards definitions
-#   for Aarakocra and Genasi from the elemental evil books
-#   
+#   for Aarakocra and Genasi from the elemental evil books  
 # --------------------------------------------------------- #
 
 AARAKOCRA = 'aarakocra'
@@ -78,52 +77,157 @@ URCHIN = 'urchin'
 # -------------------------------------------------------- #
 
 CLASSES = [
-    BARBARIAN,
-    BARD,
-    CLERIC,
-    DRUID,
-    FIGHTER,
-    MONK,
-    PALADIN,
-    RANGER,
-    ROGUE,
-    SORCERER,
-    WARLOCK,
-    WIZARD
+    BARBARIAN, BARD, CLERIC, DRUID, FIGHTER, MONK,
+    PALADIN, RANGER, ROGUE, SORCERER, WARLOCK, WIZARD
 ]
 
-BASE_RACES = [
-    DWARF, ELF, HUMAN, HALFLING
-]
-EXOTIC_RACES = [    # this distinction is made in the DMG
-    DRAGONBORN, GNOME, HALF_ELF, HALF_ORC, TIEFLING
-]
-VOLOS_RACES = [
-    AASIMAR, FIRBOLGS, GOLIATH, KENKU, LIZARDFOLK, TABAXI, TRITON
-]
-VOLOS_MONSTER_RACES = [
-    BUGBEAR, GOBLIN, HOBGOBLIN, ORC, YUAN_TI_PUREBLOOD
-]
-ELEMENTAL_EVIL_RACES = [
-    AARAKOCRA, GENASI
-]
+BASE_RACES = [DWARF, ELF, HUMAN, HALFLING]
+EXOTIC_RACES = [DRAGONBORN, GNOME, HALF_ELF, HALF_ORC, TIEFLING]
+VOLOS_RACES = [AASIMAR, FIRBOLGS, GOLIATH, KENKU, LIZARDFOLK, TABAXI, TRITON]
+VOLOS_MONSTER_RACES = [BUGBEAR, GOBLIN, HOBGOBLIN, ORC, YUAN_TI_PUREBLOOD]
+ELEMENTAL_EVIL_RACES = [AARAKOCRA, GENASI]
+
 RACES = BASE_RACES + EXOTIC_RACES + \
         VOLOS_RACES + VOLOS_MONSTER_RACES + \
         ELEMENTAL_EVIL_RACES
 
 BACKGROUNDS = [
-    ACOLYTE,
-    CHARLATAN,
-    CRIMINAL,
-    ENTERTAINER,
-    FOLK_HERO,
-    GUILD_ARTISAN,
-    HERMIT,
-    NOBLE,
-    OUTLANDER,
-    SAGE,
-    SAILOR,
-    SOLDIER,
-    URCHIN
+    ACOLYTE, CHARLATAN, CRIMINAL, ENTERTAINER, FOLK_HERO, GUILD_ARTISAN,
+    HERMIT, NOBLE, OUTLANDER, SAGE, SAILOR, SOLDIER, URCHIN
 ]
 
+
+# -------------------------------------------------------- #
+#   mappings
+# -------------------------------------------------------- #
+
+CORE_SUBCLASSES_BY_CLASS = {
+    BARBARIAN: [
+        'Path of the Berserker',
+        'Path of the Totem Warrior'
+    ],
+    BARD: [
+        'College of Lore',
+        'College of Valor'
+    ],
+    CLERIC: [
+        'Knowledge Domain',
+        'Life Domain',
+        'Light Domain',
+        'Nature Domain',
+        'Tempest Domain',
+        'Trickery Domain',
+        'War Domain',
+        'Death Domain'  # from DMG, which is 'core'
+    ],
+    DRUID: [
+        'Circle of the Land',
+        'Circle of the Moon'
+    ],
+    FIGHTER: [
+        'Champion',
+        'Battle Master',
+        'Eldritch Knight'
+    ],
+    MONK: [
+        'Way of the Open Hand',
+        'Way of Shadow',
+        'Way of the Four Elements'
+    ],
+    PALADIN: [
+        'Oath of Devotion',
+        'Oath of the Ancients',
+        'Oath of Vengeance'
+    ],
+    RANGER: [
+        'Hunter',
+        'Beast Master'
+    ],
+    ROGUE: [
+        'Thief',
+        'Assassin',
+        'Arcane Trickster'
+    ],
+    SORCERER: [
+        'Wild Magic',
+        'Draconic Bloodline'
+    ],
+    WARLOCK: [
+        'The Archfey',
+        'The Fiend',
+        'The Great Old One'
+    ],
+    WIZARD: [
+        'School of Abjuration',
+        'School of Conjuration',
+        'School of Divination',
+        'School of Enchantment',
+        'School of Evocation',
+        'School of Illusion',
+        'School of Necromancy',
+        'School of Transmutation'
+    ]
+}
+
+XAN_SUBCLASSES_BY_CLASS = {
+    BARBARIAN: [
+        'Path of the Ancestral Guardian',
+        'Path of the Storm Herald',
+        'Path of the Zealot'
+    ],
+    BARD: [
+        'College of Glamour',
+        'College of Swords',
+        'College of Whispers'
+    ],
+    CLERIC: [
+        'Forge Domain',
+        'Grave Domain'
+    ],
+    DRUID: [
+        'Circle of Dreams',
+        'Circle of the Shepherd'
+    ],
+    FIGHTER: [
+        'Arcane Archer',
+        'Cavalier',
+        'Samurai'
+    ],
+    MONK: [
+        'Way of the Drunken Master',
+        'Way of the Kensei',
+        'Way of the Sun Soul'
+    ],
+    PALADIN: [
+        'Oath of Conquest',
+        'Oath of Redemption'
+    ],
+    RANGER: [
+        'Gloom Stalker',
+        'Horizon Walker',
+        'Monster Slayer'
+    ],
+    ROGUE: [
+        'Inquisitive',
+        'Mastermind',
+        'Scout',
+        'Swashbuckler',
+    ],
+    SORCERER: [
+        'Divine Soul',
+        'Shadow Magic',
+        'Storm Sorcery'
+    ],
+    WARLOCK: [
+        'The Celestial',
+        'The Hexblade'
+    ],
+    WIZARD: [
+        'War Magic'
+    ]
+}
+
+SUBCLASSES_BY_CLASS = {
+    class_: CORE_SUBCLASSES_BY_CLASS[class_] + XAN_SUBCLASSES_BY_CLASS[class_]
+    for class_ in CLASSES
+}
